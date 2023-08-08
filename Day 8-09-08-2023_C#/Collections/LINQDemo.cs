9d84b31f14b482fd61a7db72ecece554ffd39f76
nandhinib-024 <141128695+nandhinib-024@users.noreply.github.com>
2023-08-08 22:22:19 +0530

    Add files via upload

---
+++ Day 8-09-08-2023_C#/Collections/LINQDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class LINQDemo
+    {
+        public void ShowResults()
+        {
+            // Data source.
+            int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+            // Query Expression.
+            int highScoreCount = (
+                                from score in scores
+                                where score > 90
+                                select score
+                                ).Count();
+            Console.WriteLine(highScoreCount);
+
+            
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                orderby score descending
+                select score;
+
+            // Execute the query to produce the results
+            foreach (int testScore in scoreQuery)
+            {
+                Console.WriteLine(testScore);
+            }
+            
+            
+          //   IEnumerable<int> highScoresQuery =
+         //   from score in scores
+        //    where score > 80
+        //    select score;
+           // int scoreCount = highScoresQuery.Count();
+            int highScoresQuery = scores.First(score => score > 70);
+            Console.WriteLine( highScoresQuery);
+           
+        }
+    }
+}
---
+++ Day 8-09-08-2023_C#/Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using Collections;
+JA Ja = new JA();
+
+Ja.disp1d();
+/*
+CollectionExp collectionexp=new CollectionExp();
+collectionexp.AddNewElement();
+Console.WriteLine(collectionexp.FindAnElement());
+
+GenericCollectionsDemo genericCollectionsDemo = new GenericCollectionsDemo();   
+genericCollectionsDemo.QueueElements();
+
+
+
+LINQDemo linqDemo = new LINQDemo();
+linqDemo.ShowResults();
+*/
