e2792c332dab6f42847259e5de77256755a41c60
nandhinib-024 <141128695+nandhinib-024@users.noreply.github.com>
2023-08-12 21:43:38 +0530

    Add files via upload

---
+++ Day 10-10-08-2023_C#/EFDB/CRUDEmp.cs
@@ -0,0 +1,50 @@
+﻿
+using EFDBF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCF1
+{
+    internal class CRUDEmp
+    {
+        public void AddNewEmp()
+        {
+            using (var context = new EmployeeMgmtContext())
+            {
+                var emp = new Emp()
+                {
+                    Empno = 1,
+                    Empname="Nandhini",
+                    Addr="Erode",
+                    Deptno=1
+
+                };
+                context.Emps.Add(emp);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateEmp()
+        {
+            var context = new EmployeeMgmtContext();
+            var selemp = context.Emps.FirstOrDefault(d => d.Empno == 10436);
+            selemp.Addr = "Dpm";
+            context.SaveChanges();
+
+
+        }
+        public void DeleteEmp()
+        {
+
+            var context = new EmployeeMgmtContext();
+            var selemp = context.Emps.FirstOrDefault(d => d.Empno == 10436);
+            context.Emps.Remove(selemp);
+            context.SaveChanges();
+        }
+    }
+
+
+}
+
--- Day 10-10-08-2023_C#/EFDB/Program.cs
+++ Day 10-10-08-2023_C#/EFDB/Program.cs
@@ -1,5 +1,11 @@
-﻿using EFDBF;
+﻿using EFCF1;
+using EFDBF;
 CRUD curd = new CRUD();
 curd.AddNewDept();
 //curd.UpdateDept();
 //curd.DeleteDept();
+CRUDEmp curdemp = new CRUDEmp();
+//curdemp.AddNewEmp();
+//curdemp.UpdateEmp();
+//curdemp.DeleteEmp();
+
